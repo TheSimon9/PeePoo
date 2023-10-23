f408abf8bc0eb7725d1c121432f049fe6c7afd98
Christian De Simone <des@phantom.local>
2023-10-23 16:23:37 +0200

    🩹 fix nullable supabase values

--- src/pages/api/submit.ts
+++ src/pages/api/submit.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
+const supabase = createClient(process.env.SUPABASE_URL ?? '', process.env.SUPABASE_KEY ?? '')
 
 export default async function handler(
     req: NextApiRequest,
